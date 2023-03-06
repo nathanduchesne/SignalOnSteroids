8ae5caa38dd7c8f591cdf6e98a18230e1b2f5fcf
Nathan Duchesne <nathan.duchesne@epfl.ch>
2023-03-06 13:50:10 +0100

    Init all for RRC

--- RRC/src/lib.rs
+++ RRC/src/lib.rs
@@ -1,3 +1,33 @@
+extern crate rc; 
+use std::collections::HashSet;
+
+use rc::{State, init_all, generate_dh, dh};
+use x25519_dalek::{PublicKey, SharedSecret, StaticSecret};
+
+pub struct RRC_State {
+    pub state: State,
+    pub hash_key: [u8;32],
+    pub hash_key_prime: [u8;32], 
+    pub S: HashSet<Vec<u8>>,
+    pub R: HashSet<Vec<u8>>,
+    pub S_ack: HashSet<Vec<u8>>,
+    pub max_num: u32
+}
+pub fn rrc_init_all() -> (RRC_State, RRC_State) {
+    // do key exchange for both hash keys
+    let alice_hash_key = generate_dh();
+    let alice_hash_key_prime = generate_dh(); 
+    let bob_hash_key = generate_dh();
+    let bob_hash_key_prime = generate_dh();
+
+    let hash_key = dh(alice_hash_key, bob_hash_key.public);
+    let hash_key_prime = dh(alice_hash_key_prime, bob_hash_key_prime.public);
+    let (mut alice_rc_state, mut bob_rc_state) = init_all();
+    let mut alice_state = RRC_State{state: alice_rc_state, hash_key: hash_key.to_bytes().clone(), hash_key_prime: hash_key_prime.to_bytes().clone(), S: HashSet::new(), R: HashSet::new(), S_ack: HashSet::new(), max_num: 0};
+    let mut bob_state = RRC_State{state: bob_rc_state, hash_key: hash_key.to_bytes(), hash_key_prime: hash_key_prime.to_bytes(), S: HashSet::new(), R: HashSet::new(), S_ack: HashSet::new(), max_num: 0};
+
+    return (alice_state, bob_state);
+}
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
