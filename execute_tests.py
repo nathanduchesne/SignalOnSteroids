7fa94465a47908301ce9653ec1683edd738024ad
Nathan Duchesne <nathan.duchesne@epfl.ch>
2023-05-18 19:18:17 +0200

    Corrected script error messages

--- execute_tests.py
+++ execute_tests.py
@@ -1,6 +1,5 @@
 import subprocess
 
-
 def run_rust_tests():
     directories = ["RC", "RRC", "mset-mu-hash", "unf-arc-based-on-rc", "unf-arc-based-on-rrc", "s-rid-rc"]
     for directory in directories:
@@ -10,9 +9,9 @@ def run_rust_tests():
             if result.returncode == 0:
                 print(f"Directory {directory} tests passed successfully.")
             else:
-                print("Rust tests failed.")
+                print(f"Directory {directory} tests failed.")
         except subprocess.CalledProcessError:
-            print("Error running Rust tests.")
+            print(f"Error occurred while running tests in directory {directory} (not the test suites fault).")
 
 
 if __name__ == "__main__":
